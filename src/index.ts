eac166279fcd5a74fa1470467a270839a8c79094
Katartismos <kingkatartismos@gmail.com>
2026-07-02 16:24:56 +0100

    Implemented GET all subjects

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 import express from "express";
+import cors from "cors";
+import subjectsRouter from "./routes/subjects";
 
 const app = express();
 const PORT = 8000;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "PUT", "POST", "DELETE"],
+    credentials: true,
+  }),
+);
+
+app.use("/api/subjects", subjectsRouter);
 
 // Root GET route
 app.get("/", (req, res) => {
